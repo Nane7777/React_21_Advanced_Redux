831c3d2c4a853696ed87fe36f24ce62b12547805
Nane <gemine_nicolas@hotmail.com>
2025-04-05 15:20:31 +0200

    05-04-25 React 21-327

---
+++ src/store/cart-slice.js
@@ -0,0 +1,30 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+    },
+    reducers: {
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            const existingItems = state.items.find(item => item.id === newItem.id);
+            if (!existingItems) {
+                state.items.push({
+                    itemId: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.name,
+                });
+            } else {
+                existingItems.quantity++;
+                existingItems.totalPrice = existingItems.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+
+        },
+    }
+});
